5b821bc2f18fef737bdbc64427b4fe336f5c7d73
AshKuraine <lpc9720@hotmail.com>
2026-06-19 19:08:19 -0500

    feature: generador de docxv39

--- SafetyReport.Handlers/DocxGeneratorService.cs
+++ SafetyReport.Handlers/DocxGeneratorService.cs
@@ -581,7 +581,7 @@ public partial class DocxGeneratorService
         secPr.Append(new PageMargin
         {
             Top = mt,
-            Bottom = mb - footerGapBefore / 2,
+            Bottom = mb - footerGapBefore * 3 / 4,
             Left = (uint)ml,
             Right = (uint)mr,
             Header = (uint)headerDistance,
