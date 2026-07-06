0b7f516b3e3f31161c91ec475ca5a7eb698b5540
AshKuraine <lpc9720@hotmail.com>
2026-07-06 15:29:41 -0500

    feature: actualizacion tabla maestrav2

--- SafetyReport.Handlers/PedidoArchivoHandler.cs
+++ SafetyReport.Handlers/PedidoArchivoHandler.cs
@@ -90,7 +90,7 @@ namespace SafetyReport.Handlers
 
             if (!string.IsNullOrWhiteSpace(mime))
             {
-                var respuesta = await _tablaMaestraDao.ListarAsync(usuarioLogueado, 34);
+                var respuesta = await _tablaMaestraDao.ListarAsync(usuarioLogueado, "34");
                 if (respuesta.IdTipoMensaje == 2 && respuesta.Result is List<TablaMaestraItem> items)
                 {
                     var match = items.FirstOrDefault(i =>
