257832fee6748a70d440a7771dcb476a847f1a7d
AshKuraine <lpc9720@hotmail.com>
2026-09-09 15:00:32 -0500

    checkpoint 38 modelsv2

--- SafetyReport.Application/Puertos/Login/CognitoLoginRequest.cs
+++ SafetyReport.Application/Puertos/Login/CognitoLoginRequest.cs
@@ -1,16 +1,16 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace SafetyReport.Models
+namespace SafetyReport.Application.Puertos.Login
 {
     public class CognitoLoginRequest
     {
         [JsonPropertyName("custom:id_empresa")]
-        public string IdEmpresa { get; set; }
+        public string IdEmpresa { get; set; } = string.Empty;
 
         [JsonPropertyName("custom:id_usuario")]
-        public string IdUsuario { get; set; }
+        public string IdUsuario { get; set; } = string.Empty;
 
         [JsonPropertyName("cognito:username")]
-        public string Usuario { get; set; }
+        public string Usuario { get; set; } = string.Empty;
     }
 }
---
+++ SafetyReport.Application/Puertos/Login/Login.cs
@@ -0,0 +1,26 @@
+﻿namespace SafetyReport.Application.Puertos.Login
+{
+    public class LoginRequest
+    {
+        public string Usuario { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string IdToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+
+    public class LoginApiResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string IdToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+
+        public int? IdUsuario { get; set; }
+        public string Usuario { get; set; } = string.Empty;
+        public int? IdEmpresa { get; set; }
+    }
+}
